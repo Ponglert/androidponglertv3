e27e4a0ba4640f04a7312124001a7db8725e4882
Ponglert <asakuratran@gmail.com>
2026-03-10 15:36:48 +0700

    uptov5

---
+++ app/src/main/java/com/ps/ponglertv3/CreateMember.java
@@ -0,0 +1,25 @@
+package com.ps.ponglertv3;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateMember extends AppCompatActivity {
+
+    private EditText idEdtMemberName, idMemberTel, idMemberEmail;
+    private Button idBtnInsert;
+    private ProgressBar idPBLoading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
