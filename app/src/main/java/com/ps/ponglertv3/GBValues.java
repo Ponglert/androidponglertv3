214aa4c5d9974ae14b2ed79a5f122218d746afce
Ponglert <asakuratran@gmail.com>
2026-03-12 10:16:54 +0700

    uptov8

--- app/src/main/java/com/ps/ponglertv3/GBValues.java
+++ app/src/main/java/com/ps/ponglertv3/GBValues.java
@@ -1,6 +1,6 @@
 package com.ps.ponglertv3;
 
 public class GBValues {
-    public static String hostname = "http://localhost/ponglertv2/";
+    public static String hostname = "http://10.30.200.106/ponglertv2/";
 }
 
